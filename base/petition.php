8159c999eb090d0638a4c03e0cd4aa3903b647ed
Bruno Bergot <bruno@eliaz.fr>
2010-06-01 18:46:13 +0000

    suite de [38484] : le plugin petitions gère l'install/desinstall de ses tables

---
+++ base/petition.php
@@ -0,0 +1,101 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2009                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+/**
+ * Interfaces des tables petitions et signatures pour le compilateur
+ *
+ * @param array $interfaces
+ * @return array
+ */
+function petition_declarer_tables_interfaces($interfaces){
+
+	$interfaces['table_des_tables']['petitions']='petitions';
+	$interfaces['table_des_tables']['signatures']='signatures';
+	
+	$interfaces['exceptions_des_tables']['signatures']['date']='date_time';
+	$interfaces['exceptions_des_tables']['signatures']['nom']='nom_email';
+	$interfaces['exceptions_des_tables']['signatures']['email']='ad_email';
+	
+	$interfaces['table_date']['signatures']='date_time';
+	
+	$interfaces['tables_jointures']['spip_articles'][]= 'petitions';
+	$interfaces['tables_jointures']['spip_articles'][]= 'signatures';
+
+	$interfaces['exceptions_des_jointures']['petition'] = array('spip_petitions', 'texte');
+	$interfaces['exceptions_des_jointures']['id_signature']= array('spip_signatures', 'id_signature');
+
+	return $interfaces;
+}
+
+/**
+ * Table principale spip_signatures
+ *
+ * @param array $tables_principales
+ * @return array
+ */
+function petition_declarer_tables_principales($tables_principales){
+
+	$spip_signatures = array(
+			"id_signature"	=> "bigint(21) NOT NULL",
+			"id_article"	=> "bigint(21) DEFAULT '0' NOT NULL",
+			"date_time"	=> "datetime DEFAULT '0000-00-00 00:00:00' NOT NULL",
+			"nom_email"	=> "text DEFAULT '' NOT NULL",
+			"ad_email"	=> "text DEFAULT '' NOT NULL",
+			"nom_site"	=> "text DEFAULT '' NOT NULL",
+			"url_site"	=> "text DEFAULT '' NOT NULL",
+			"message"	=> "mediumtext DEFAULT '' NOT NULL",
+			"statut"	=> "varchar(10) DEFAULT '0' NOT NULL",
+			"maj"	=> "TIMESTAMP");
+
+	$spip_signatures_key = array(
+			"PRIMARY KEY"	=> "id_signature",
+			"KEY id_article"	=> "id_article",
+			"KEY statut" => "statut");
+	$spip_signatures_join = array(
+			"id_signature"=>"id_signature",
+			"id_article"=>"id_article");
+
+	$tables_principales['spip_signatures'] =
+		array('field' => &$spip_signatures,	'key' => &$spip_signatures_key, 'join' => &$spip_signatures_join);
+
+	return $tables_principales;
+}
+
+/**
+ * Table auxilaire spip_petitions
+ *
+ * @param array $tables_principales
+ * @return array
+ */
+function petition_declarer_tables_auxiliaires($tables_auxiliaires){
+
+	$spip_petitions = array(
+			"id_article"	=> "bigint(21) DEFAULT '0' NOT NULL",
+			"email_unique"	=> "CHAR (3) DEFAULT '' NOT NULL",
+			"site_obli"	=> "CHAR (3) DEFAULT '' NOT NULL",
+			"site_unique"	=> "CHAR (3) DEFAULT '' NOT NULL",
+			"message"	=> "CHAR (3) DEFAULT '' NOT NULL",
+			"texte"	=> "LONGTEXT DEFAULT '' NOT NULL",
+			"maj"	=> "TIMESTAMP");
+
+	$spip_petitions_key = array(
+			"PRIMARY KEY"	=> "id_article");
+
+
+	$tables_auxiliaires['spip_petitions'] = array(
+		'field' => &$spip_petitions,
+		'key' => &$spip_petitions_key);
+
+	return $tables_auxiliaires;
+}
+
+?>
---
+++ base/petition_upgrade.php
@@ -0,0 +1,48 @@
+<?php
+
+/***************************************************************************\
+ *  SPIP, Systeme de publication pour l'internet                           *
+ *                                                                         *
+ *  Copyright (c) 2001-2009                                                *
+ *  Arnaud Martin, Antoine Pitrou, Philippe Riviere, Emmanuel Saint-James  *
+ *                                                                         *
+ *  Ce programme est un logiciel libre distribue sous licence GNU/GPL.     *
+ *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
+\***************************************************************************/
+
+/**
+ * Installation/maj des tables petitions et signatures
+ *
+ * @param string $nom_meta_base_version
+ * @param string $version_cible
+ */
+function petition_upgrade($nom_meta_base_version,$version_cible){
+	$current_version = 0.0;
+	if (   (!isset($GLOBALS['meta'][$nom_meta_base_version]) )
+			|| (($current_version = $GLOBALS['meta'][$nom_meta_base_version])!=$version_cible)){
+
+		if ($current_version==0.0){
+			include_spip('base/create');
+			// creer les tables
+			creer_base();
+			// mettre les metas par defaut
+			$config = charger_fonction('config','inc');
+			$config();
+			ecrire_meta($nom_meta_base_version,$current_version=$version_cible);
+		}
+	}
+}
+
+/**
+ * Desinstallation/suppression des tables petitions et signatures
+ *
+ * @param string $nom_meta_base_version
+ */
+function petition_vider_tables($nom_meta_base_version) {
+	sql_drop_table("spip_petitions");
+	sql_drop_table("spip_signatures");
+
+	effacer_meta($nom_meta_base_version);
+}
+
+?>
