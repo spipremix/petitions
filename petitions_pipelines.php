b3ccf6dc182d3ad4b3f48b2955372fa9bb0295d5
kent1 <kent1@arscenic.info>
2011-10-10 15:18:46 +0000

    Montrons le bon exemple :
    if (!defined('_ECRIRE_INC_VERSION')) return; partout

--- petitions_pipelines.php
+++ petitions_pipelines.php
@@ -10,7 +10,8 @@
  *  Pour plus de details voir le fichier COPYING.txt ou l'aide en ligne.   *
 \***************************************************************************/
 
- 
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
 /**
  * Boite de configuration des objets articles
  *
