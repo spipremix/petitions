bb635b51102cd197a5142c35d911769e53fe816c
davoodhossein <davoodhossein@yahoo.com>
2012-03-26 08:45:34 +0000

    langues (petitions)

--- lang/petitions_fa.php
+++ lang/petitions_fa.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org/tradlang_module/petitions?lang_cible=fa
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,22 +8,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'alerte_confirmer_suppression_signatures' => 'Vous allez supprimer toutes les signatures de cette p&eacute;tition. &Ecirc;tes-vous sur de vouloir continuer ?', # NEW
-	'aucune_signature' => 'Aucune signature', # NEW
+	'alerte_confirmer_suppression_signatures' => 'شما داريد تمام امضاهاي اين طومار را حذف مي‌كنيد. مطمئن هستيد كه مي‌خواهيد ادامه دهيد؟',
+	'aucune_signature' => 'هيچ امضا',
 
 	// B
-	'bouton_checkbox_envoi_message' => 'possibilit&eacute; d\'envoyer un message', # NEW
-	'bouton_checkbox_indiquer_site' => 'indiquer obligatoirement un site web', # NEW
-	'bouton_checkbox_signature_unique_email' => 'une seule signature par adresse email', # NEW
-	'bouton_checkbox_signature_unique_site' => 'une seule signature par site web', # NEW
+	'bouton_checkbox_envoi_message' => 'امكان ارسال يك پيام',
+	'bouton_checkbox_indiquer_site' => 'معرفي الزامي يك وب سايت',
+	'bouton_checkbox_signature_unique_email' => 'فقط يك امضا براي نشاني ايميل ',
+	'bouton_checkbox_signature_unique_site' => 'فقط يك امضا براي يك وب سايت',
 
 	// F
-	'filtre' => 'Filtre :', # NEW
-	'form_pet_adresse_site' => 'آدرس سايت تان', # MODIF
-	'form_pet_aucune_signature' => '...هيچ امضائى با اين كد مطابقت نميكند  ',
+	'filtre' => 'فيتلر:',
+	'form_pet_adresse_site' => 'نشاني يوآر.ال سايت شما',
+	'form_pet_aucune_signature' => '...هيچ امضائى با اين كد مطابقت نمي‌كند  ',
 	'form_pet_confirmation' => ':خواهشمند است امضاء تان را تأئيد كنيد',
 	'form_pet_deja_signe' => '.شما قبلأ اين متن را امضاء كرده ايد',
-	'form_pet_envoi_mail_confirmation' => 'يك نامه الكترونكي براى تأئيد @email@ برايتان فرستاده شده. شما بايد از آدرس ذكر شده براى تأئيد امضاء تان ديدن نمائيد', # MODIF
+	'form_pet_envoi_mail_confirmation' => 'يك نامه الكترونيكي تأئيد به @email@ برايتان فرستاده شده. شما براي تأييد امضاي خود بايد به نشاني وب ذكر شده در آن نامه مراجعه نمائيد.',
 	'form_pet_mail_confirmation' => 'سلام,
  
  شما تقاضاى امضاء يك درخواست راكرده ايد    @titre@.
@@ -38,69 +38,69 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 از شركتتان متشكريم
  ',
-	'form_pet_message_commentaire' => 'يك پيام ، يك تفسير ؟', # MODIF
-	'form_pet_nom_site2' => 'نام سايتتان<b>      ', # MODIF
+	'form_pet_message_commentaire' => 'يك نظر؟',
+	'form_pet_nom_site2' => 'نام وب سايت شما      ',
 	'form_pet_probleme_liens' => 'خواهشمند است پیامتان را با حذف پیوند هایپرتکت دریافت کنید.',
 	'form_pet_probleme_technique' => 'نقص فنى، امضاء ها موقتأ لغو شدند',
 	'form_pet_signature_validee' => 'امضاء تان تأئيد شد. متشكريم !',
 	'form_pet_site_deja_enregistre' => 'اين سايت قبلأ ثبت شده',
 	'form_pet_url_invalide' => 'كه مشخص كرده ايد معتبر نميباشد URL ',
 	'form_pet_votre_email' => 'آدرس ايميل تان',
-	'form_pet_votre_nom' => 'نام يا نام مستعارتان', # MODIF
+	'form_pet_votre_nom' => 'نام شما',
 	'form_pet_votre_site' => 'اگر داراى سايتى هستيد، ميتوانيد آنرا در زير ذكر كنيد',
 
 	// I
-	'icone_relancer_signataire' => 'Relancer le signataire', # NEW
-	'icone_relancer_signataires' => 'Relancer les signataires', # NEW
-	'icone_suivi_petitions' => 'Suivre/g&eacute;rer les p&eacute;titions', # NEW
-	'icone_supprimer_signature' => 'Supprimer cette signature', # NEW
-	'icone_supprimer_signatures' => 'Supprimer les signatures', # NEW
-	'icone_valider_signature' => 'Valider cette signature', # NEW
-	'icone_valider_signatures' => 'Valider les signatures', # NEW
+	'icone_relancer_signataire' => 'بازنشر امضا كننده',
+	'icone_relancer_signataires' => 'بازنشر امضاءكنندگان',
+	'icone_suivi_petitions' => 'پيگيري/اداره طومارها',
+	'icone_supprimer_signature' => 'حذف اين امضا',
+	'icone_supprimer_signatures' => 'حذف امضاها',
+	'icone_valider_signature' => 'تأييد اين امضا',
+	'icone_valider_signatures' => 'تأييد امضاها',
 	'info_adresse_email' => ': آدرس ايميل',
-	'info_fonctionnement_petition' => 'Fonctionnement de la p&eacute;tition :', # NEW
+	'info_fonctionnement_petition' => 'عملكرد طومار:',
 	'info_signature_supprimee' => 'امضا حذف شده',
-	'info_signature_supprimee_erreur' => 'اشتباه: این کد حذفی به هیچ امضائی مربوط نیست',
+	'info_signature_supprimee_erreur' => 'خطا: این کد حذف با هیچ امضائی منطبق نيست',
 	'info_site_web' => ':تارنمای سايت ',
-	'info_texte_message' => ':متن پيامتان',
+	'info_texte_message' => 'متن پيام شما',
 
 	// L
 	'lien_reponse_article' => 'پاسخ به مقاله',
 
 	// N
-	'nombre_signatures' => '@nb@ signatures', # NEW
+	'nombre_signatures' => 'امضا@nb@',
 
 	// S
-	'sans_nom' => 'Anonyme', # NEW
-	'signatures_article' => 'Signatures de l\'article :', # NEW
-	'signatures_aucune' => 'Aucune', # NEW
-	'signatures_meme_auteur' => 'Les signatures du m', # NEW
-	'signatures_meme_site' => 'Les signatures avec le m', # NEW
-	'signatures_poubelle' => 'Supprim&eacute;es', # NEW
-	'signatures_prop' => 'En attente', # NEW
-	'signatures_publie' => 'Confirm&eacute;es', # NEW
-	'signatures_recherche_label' => 'Rechercher des signatures&nbsp;:', # NEW
-	'signatures_toutes' => 'Toutes', # NEW
-	'statut_poubelle' => 'Supprim&eacute;e', # NEW
-	'statut_prop' => 'En attente de validation', # NEW
-	'statut_publie' => 'Confirm&eacute;e', # NEW
+	'sans_nom' => 'بدون امضا',
+	'signatures_article' => 'امضاهاي مقاله:',
+	'signatures_aucune' => 'هيچ',
+	'signatures_meme_auteur' => 'امضاهاي همان نويسنده',
+	'signatures_meme_site' => 'امضاها با همان سايت',
+	'signatures_poubelle' => 'حذفي‌ها',
+	'signatures_prop' => 'در انتظار',
+	'signatures_publie' => 'تأييد شده‌ها',
+	'signatures_recherche_label' => 'جستجوي امضاها:',
+	'signatures_toutes' => 'همه',
+	'statut_poubelle' => 'به سبدباطله',
+	'statut_prop' => 'در انتظار تأييد',
+	'statut_publie' => 'تأييد شده',
 
 	// T
-	'texte_descriptif_petition' => 'Descriptif de la p&eacute;tition', # NEW
-	'titre_page_controle_petition' => 'Suivi des p&eacute;titions', # NEW
-	'titre_petition' => 'P&eacute;tition', # NEW
-	'titre_petitions' => 'P&eacute;titions', # NEW
-	'titre_selection_action' => 'Sélection', # NEW
-	'titre_signatures_attente' => 'Signatures en attente de validation', # NEW
-	'titre_signatures_publie' => 'Signatures confirm&eacute;es', # NEW
-	'titre_suivi_petition' => 'Suivi des p&eacute;titions', # NEW
-	'tout_voir' => 'Voir toutes les signatures', # NEW
+	'texte_descriptif_petition' => 'توصيف طومار',
+	'titre_page_controle_petition' => 'پيگيري طومارها',
+	'titre_petition' => 'طومار',
+	'titre_petitions' => 'طومارها (درخواست‌ها)',
+	'titre_selection_action' => 'گزينش',
+	'titre_signatures_attente' => 'امضاهاي در انتظار تأييد',
+	'titre_signatures_publie' => 'امضاهاي تأييد شده',
+	'titre_suivi_petition' => 'پيگيري درخواست‌ها (طومارها)',
+	'tout_voir' => 'ديدن تمام امضاها',
 
 	// U
-	'une_signature' => '1 signature', # NEW
+	'une_signature' => '1 امضا',
 
 	// V
-	'voir_signatures_objet' => 'Voir les signatures' # NEW
+	'voir_signatures_objet' => 'ديدن امضاها'
 );
 
 ?>
