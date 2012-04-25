4fde414c2f05b3d1ae808ce9c587ab8d716c4611
elvis.deltedesco <elvis.deltedesco@phante.com>
2012-04-25 06:37:18 +0000

    langues (petitions)

--- lang/petitions_it.php
+++ lang/petitions_it.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org/tradlang_module/petitions?lang_cible=it
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,25 +8,25 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'alerte_confirmer_suppression_signatures' => 'Vous allez supprimer toutes les signatures de cette p&eacute;tition. &Ecirc;tes-vous sur de vouloir continuer ?', # NEW
-	'aucune_signature' => 'Aucune signature', # NEW
+	'alerte_confirmer_suppression_signatures' => 'Stai per cancellare tutte le adesioni a questa petizione. Sei sicuro di voler continuare?',
+	'aucune_signature' => 'Nessuna firma',
 
 	// B
-	'bouton_checkbox_envoi_message' => 'possibilit&eacute; d\'envoyer un message', # NEW
-	'bouton_checkbox_indiquer_site' => 'indiquer obligatoirement un site web', # NEW
-	'bouton_checkbox_signature_unique_email' => 'une seule signature par adresse email', # NEW
-	'bouton_checkbox_signature_unique_site' => 'une seule signature par site web', # NEW
+	'bouton_checkbox_envoi_message' => 'possibilità di inviare un messaggio',
+	'bouton_checkbox_indiquer_site' => 'devi inserire il nome di un sito web',
+	'bouton_checkbox_signature_unique_email' => 'solo un\'adesione per email',
+	'bouton_checkbox_signature_unique_site' => 'solo una adesione per sito web',
 
 	// F
-	'filtre' => 'Filtre :', # NEW
-	'form_pet_adresse_site' => 'Indirizzo del sito', # MODIF
-	'form_pet_aucune_signature' => 'Nessuna adesione corrisponde a questo codice...',
+	'filtre' => 'Filtro:',
+	'form_pet_adresse_site' => 'Indirizzo del sito',
+	'form_pet_aucune_signature' => 'A questo codice non corrisponde nessuna desione ...',
 	'form_pet_confirmation' => 'Conferma l\'adesione:',
 	'form_pet_deja_signe' => 'Hai già firmato questo testo.',
-	'form_pet_envoi_mail_confirmation' => 'Ti è stato appena inviato un messaggio di posta elettronica all\'indirizzo @email@. Dovrai visitare l\'indirizzo web che appare nel messaggio per confermare la tua adesione.', # MODIF
+	'form_pet_envoi_mail_confirmation' => 'Ti è stato appena inviato un messaggio di posta elettronica all\'indirizzo @email@. Dovrai visitare l\'indirizzo web che appare nel messaggio per confermare la tua adesione.',
 	'form_pet_mail_confirmation' => 'Salve,
 
-Hai richiesto di firmare la petizione:
+Hai richiesto di aderire alla petizione:
 @titre@.
 
 Hai fornito le seguenti informazioni:
@@ -42,69 +42,69 @@ Per confermare la tua adesione è sufficiente visitare l\'indirizzo web qui sott
 
 
 Grazie per la partecipazione',
-	'form_pet_message_commentaire' => 'Un messaggio, un commento?', # MODIF
-	'form_pet_nom_site2' => 'Nome del tuo sito Web', # MODIF
+	'form_pet_message_commentaire' => 'Un messaggio, un commento?',
+	'form_pet_nom_site2' => 'Nome del tuo sito web',
 	'form_pet_probleme_liens' => 'Per favore, riprenda il suo messaggio ed elimini i link ipertestuali.',
 	'form_pet_probleme_technique' => 'Le adesioni sono temporaneamente sospese per il verificarsi di problemi tecnici.',
 	'form_pet_signature_validee' => 'La tua adesione è stata confermata. Grazie!',
 	'form_pet_site_deja_enregistre' => 'Questo sito è già registrato',
 	'form_pet_url_invalide' => 'L\'URL indicato non risulta valido.',
 	'form_pet_votre_email' => 'Il tuo indirizzo email',
-	'form_pet_votre_nom' => 'Nome o pseudonimo', # MODIF
+	'form_pet_votre_nom' => 'Il tuo nome',
 	'form_pet_votre_site' => 'Se hai un sito Web, puoi segnalarlo qui sotto',
 
 	// I
-	'icone_relancer_signataire' => 'Relancer le signataire', # NEW
-	'icone_relancer_signataires' => 'Relancer les signataires', # NEW
-	'icone_suivi_petitions' => 'Suivre/g&eacute;rer les p&eacute;titions', # NEW
-	'icone_supprimer_signature' => 'Supprimer cette signature', # NEW
-	'icone_supprimer_signatures' => 'Supprimer les signatures', # NEW
-	'icone_valider_signature' => 'Valider cette signature', # NEW
-	'icone_valider_signatures' => 'Valider les signatures', # NEW
+	'icone_relancer_signataire' => 'Contatta il firmatario di nuovo',
+	'icone_relancer_signataires' => 'Contatta di nuovo i firmatari',
+	'icone_suivi_petitions' => 'Segui/gestisci le petizioni',
+	'icone_supprimer_signature' => 'Cancella questa adesione',
+	'icone_supprimer_signatures' => 'Cancella le adesioni',
+	'icone_valider_signature' => 'Conferma l\'adesione',
+	'icone_valider_signatures' => 'Conferma le adesioni',
 	'info_adresse_email' => 'INDIRIZZO EMAIL:',
-	'info_fonctionnement_petition' => 'Fonctionnement de la p&eacute;tition :', # NEW
+	'info_fonctionnement_petition' => 'Come funzionano le petizioni:',
 	'info_signature_supprimee' => 'Firma eliminata',
 	'info_signature_supprimee_erreur' => 'Errore: questo codice di eliminazione non corrisponde a nessuna firma',
 	'info_site_web' => 'SITO WEB:',
-	'info_texte_message' => 'Testo del messaggio:',
+	'info_texte_message' => 'Testo del tuo messaggio:',
 
 	// L
 	'lien_reponse_article' => 'Risposta all\'articolo',
 
 	// N
-	'nombre_signatures' => '@nb@ signatures', # NEW
+	'nombre_signatures' => '@nb@ adesioni',
 
 	// S
-	'sans_nom' => 'Anonyme', # NEW
-	'signatures_article' => 'Signatures de l\'article :', # NEW
-	'signatures_aucune' => 'Aucune', # NEW
-	'signatures_meme_auteur' => 'Les signatures du m', # NEW
-	'signatures_meme_site' => 'Les signatures avec le m', # NEW
-	'signatures_poubelle' => 'Supprim&eacute;es', # NEW
-	'signatures_prop' => 'En attente', # NEW
-	'signatures_publie' => 'Confirm&eacute;es', # NEW
-	'signatures_recherche_label' => 'Rechercher des signatures&nbsp;:', # NEW
-	'signatures_toutes' => 'Toutes', # NEW
-	'statut_poubelle' => 'Supprim&eacute;e', # NEW
-	'statut_prop' => 'En attente de validation', # NEW
-	'statut_publie' => 'Confirm&eacute;e', # NEW
+	'sans_nom' => 'Anonimo',
+	'signatures_article' => 'Firmatari dell\'articolo:',
+	'signatures_aucune' => 'Nessuna',
+	'signatures_meme_auteur' => 'Adesioni dello stesso autore',
+	'signatures_meme_site' => 'Adesioni con lo stesso sito web',
+	'signatures_poubelle' => 'Cancellata',
+	'signatures_prop' => 'Attendere',
+	'signatures_publie' => 'Confermata',
+	'signatures_recherche_label' => 'Ricercare per adesione:',
+	'signatures_toutes' => 'Tutte',
+	'statut_poubelle' => 'Cancellata',
+	'statut_prop' => 'Aspettando la validazione',
+	'statut_publie' => 'Confermata',
 
 	// T
-	'texte_descriptif_petition' => 'Descriptif de la p&eacute;tition', # NEW
-	'titre_page_controle_petition' => 'Suivi des p&eacute;titions', # NEW
-	'titre_petition' => 'P&eacute;tition', # NEW
-	'titre_petitions' => 'P&eacute;titions', # NEW
-	'titre_selection_action' => 'Sélection', # NEW
-	'titre_signatures_attente' => 'Signatures en attente de validation', # NEW
-	'titre_signatures_publie' => 'Signatures confirm&eacute;es', # NEW
-	'titre_suivi_petition' => 'Suivi des p&eacute;titions', # NEW
-	'tout_voir' => 'Voir toutes les signatures', # NEW
+	'texte_descriptif_petition' => 'Descrizione della petizione',
+	'titre_page_controle_petition' => 'Tracciatura della petizione',
+	'titre_petition' => 'Petizione',
+	'titre_petitions' => 'Petizioni',
+	'titre_selection_action' => 'Selezione',
+	'titre_signatures_attente' => 'Adesioni in attesa di valisazione',
+	'titre_signatures_publie' => 'Conferma adesioni',
+	'titre_suivi_petition' => 'Tracciatura delle petizioni',
+	'tout_voir' => 'Mostra tutte le adesioni',
 
 	// U
-	'une_signature' => '1 signature', # NEW
+	'une_signature' => '1 adesione',
 
 	// V
-	'voir_signatures_objet' => 'Voir les signatures' # NEW
+	'voir_signatures_objet' => 'Mostra le adesioni'
 );
 
 ?>
