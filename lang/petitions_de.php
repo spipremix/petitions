1b41c106f954f649e6db859381a52458ee31a29d
klaus <klaus@spip.de>
2012-05-25 01:38:50 +0000

    langues (petitions)

--- lang/petitions_de.php
+++ lang/petitions_de.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// extrait automatiquement de http://www.spip.net/trad-lang/
+// extrait automatiquement de http://trad.spip.org/tradlang_module/petitions?lang_cible=de
 // ** ne pas modifier le fichier **
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
@@ -8,22 +8,22 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// A
-	'alerte_confirmer_suppression_signatures' => 'Vous allez supprimer toutes les signatures de cette p&eacute;tition. &Ecirc;tes-vous sur de vouloir continuer ?', # NEW
-	'aucune_signature' => 'Aucune signature', # NEW
+	'alerte_confirmer_suppression_signatures' => 'Sie sind dabei, alle Unterzeichner dieser Petition zu löschen. Sind Sie sicher?',
+	'aucune_signature' => 'Keine Unterzeicher',
 
 	// B
-	'bouton_checkbox_envoi_message' => 'possibilit&eacute; d\'envoyer un message', # NEW
-	'bouton_checkbox_indiquer_site' => 'indiquer obligatoirement un site web', # NEW
-	'bouton_checkbox_signature_unique_email' => 'une seule signature par adresse email', # NEW
-	'bouton_checkbox_signature_unique_site' => 'une seule signature par site web', # NEW
+	'bouton_checkbox_envoi_message' => 'Möglichkeit zum Senden von Nachrichten',
+	'bouton_checkbox_indiquer_site' => 'Angabe einer Website erforderlich',
+	'bouton_checkbox_signature_unique_email' => 'eine einzige Unterzeichnung pro Email',
+	'bouton_checkbox_signature_unique_site' => 'nur ein Unterzeichner pro Website',
 
 	// F
-	'filtre' => 'Filtre :', # NEW
-	'form_pet_adresse_site' => 'Adresse Ihrer Website', # MODIF
+	'filtre' => 'Filter:',
+	'form_pet_adresse_site' => 'Adresse (URL) Ihrer Website',
 	'form_pet_aucune_signature' => 'Diesem Code entspricht keine Signatur...',
 	'form_pet_confirmation' => 'Bitte bestätigen Sie Ihre Unterschrift:',
 	'form_pet_deja_signe' => 'Sie haben diesen Text bereits unterschrieben.',
-	'form_pet_envoi_mail_confirmation' => 'Eine E-mail mit den Daten zum Bestätigen wurde an @email@ geschickt. Sie müssen die dort angegebene WWW-Seite öffnen, um Ihre Unterschrift zu bestätigen.', # MODIF
+	'form_pet_envoi_mail_confirmation' => 'Eine E-mail wurde zur Prüfung Ihrer Identität an @email@ geschickt. Sie müssen die dort angegebene WWW-Seite öffnen, um Ihre Unterschrift zu bestätigen.',
 	'form_pet_mail_confirmation' => 'Hallo,
 
 Sie möchten diese Petition unterzeichnen:
@@ -42,27 +42,27 @@ Um Ihre Unterschrift zu bestätigen müssen Sie diese Seite aufrufen (wenn Sie e
 
 Vielen Dank für Ihre Teilnahme
 ',
-	'form_pet_message_commentaire' => 'Eine Nachricht, ein Kommentar?', # MODIF
-	'form_pet_nom_site2' => 'Name Ihrer Website', # MODIF
+	'form_pet_message_commentaire' => 'Eine Nachricht oder ein Kommentar?',
+	'form_pet_nom_site2' => 'Name Ihrer Website',
 	'form_pet_probleme_liens' => 'Bitte entfernen Sie die Hyperlinks aus Ihrer Nachricht.',
 	'form_pet_probleme_technique' => 'Technisches Problem: Signaturen sind vorübergehend nicht möglich.',
 	'form_pet_signature_validee' => 'Ihre Signatur wurde erfolgreich geprüft. Vielen Dank!',
 	'form_pet_site_deja_enregistre' => 'Diese Website wurde bereits erfasst',
 	'form_pet_url_invalide' => 'Sie haben einen ungültigen URL angegeben.',
 	'form_pet_votre_email' => 'Ihre E-Mail Adresse',
-	'form_pet_votre_nom' => 'Ihr Name oder Pseudonym', # MODIF
+	'form_pet_votre_nom' => 'Ihr Name',
 	'form_pet_votre_site' => 'Wenn Sie eine eigene Website haben, können Sie sie hier eintragen',
 
 	// I
-	'icone_relancer_signataire' => 'Relancer le signataire', # NEW
-	'icone_relancer_signataires' => 'Relancer les signataires', # NEW
-	'icone_suivi_petitions' => 'Suivre/g&eacute;rer les p&eacute;titions', # NEW
-	'icone_supprimer_signature' => 'Supprimer cette signature', # NEW
-	'icone_supprimer_signatures' => 'Supprimer les signatures', # NEW
-	'icone_valider_signature' => 'Valider cette signature', # NEW
-	'icone_valider_signatures' => 'Valider les signatures', # NEW
+	'icone_relancer_signataire' => 'Unterzeichner neu initialisieren',
+	'icone_relancer_signataires' => 'Die Unterzeichner neu initialisieren',
+	'icone_suivi_petitions' => 'Petitionen verwalten/verfolgen',
+	'icone_supprimer_signature' => 'Diese Unterschrift löschen',
+	'icone_supprimer_signatures' => 'Unterzeichner löschen',
+	'icone_valider_signature' => 'Unterzeichnung bestätigen',
+	'icone_valider_signatures' => 'Unterzeichner bestätigen',
 	'info_adresse_email' => 'E-MAIL ADRESSE:',
-	'info_fonctionnement_petition' => 'Fonctionnement de la p&eacute;tition :', # NEW
+	'info_fonctionnement_petition' => 'Funktionsweise der Petition:',
 	'info_signature_supprimee' => 'Unterschrift gelöscht',
 	'info_signature_supprimee_erreur' => 'Fehler: Dieser Löschcode entspricht keiner Unterschrift.',
 	'info_site_web' => 'WEBSITE:',
@@ -72,39 +72,39 @@ Vielen Dank für Ihre Teilnahme
 	'lien_reponse_article' => 'Antwort auf Artikel',
 
 	// N
-	'nombre_signatures' => '@nb@ signatures', # NEW
+	'nombre_signatures' => '@nb@ Unterzeichner',
 
 	// S
-	'sans_nom' => 'Anonyme', # NEW
-	'signatures_article' => 'Signatures de l\'article :', # NEW
-	'signatures_aucune' => 'Aucune', # NEW
-	'signatures_meme_auteur' => 'Les signatures du m', # NEW
-	'signatures_meme_site' => 'Les signatures avec le m', # NEW
-	'signatures_poubelle' => 'Supprim&eacute;es', # NEW
-	'signatures_prop' => 'En attente', # NEW
-	'signatures_publie' => 'Confirm&eacute;es', # NEW
-	'signatures_recherche_label' => 'Rechercher des signatures&nbsp;:', # NEW
-	'signatures_toutes' => 'Toutes', # NEW
-	'statut_poubelle' => 'Supprim&eacute;e', # NEW
-	'statut_prop' => 'En attente de validation', # NEW
-	'statut_publie' => 'Confirm&eacute;e', # NEW
+	'sans_nom' => 'Anonym',
+	'signatures_article' => 'Unterzeichner des Artikels:',
+	'signatures_aucune' => 'Kein',
+	'signatures_meme_auteur' => 'Unterschriften des gleichen Unterzeichners',
+	'signatures_meme_site' => 'Unterzeichner der gleichen Website',
+	'signatures_poubelle' => 'Gelöscht',
+	'signatures_prop' => 'In der Warteschlange',
+	'signatures_publie' => 'Bestätigt',
+	'signatures_recherche_label' => 'Unterzeichner suchen:',
+	'signatures_toutes' => 'Alle',
+	'statut_poubelle' => 'Gelöscht',
+	'statut_prop' => 'Warten auf Freischaltung',
+	'statut_publie' => 'Bestätigt',
 
 	// T
-	'texte_descriptif_petition' => 'Descriptif de la p&eacute;tition', # NEW
-	'titre_page_controle_petition' => 'Suivi des p&eacute;titions', # NEW
-	'titre_petition' => 'P&eacute;tition', # NEW
-	'titre_petitions' => 'P&eacute;titions', # NEW
-	'titre_selection_action' => 'Sélection', # NEW
-	'titre_signatures_attente' => 'Signatures en attente de validation', # NEW
-	'titre_signatures_publie' => 'Signatures confirm&eacute;es', # NEW
-	'titre_suivi_petition' => 'Suivi des p&eacute;titions', # NEW
-	'tout_voir' => 'Voir toutes les signatures', # NEW
+	'texte_descriptif_petition' => 'Beschreibung der Petition',
+	'titre_page_controle_petition' => 'Verfolgen der Petitionen',
+	'titre_petition' => 'Petition',
+	'titre_petitions' => 'Petitionen',
+	'titre_selection_action' => 'Auswahl',
+	'titre_signatures_attente' => 'Warteschlange der Unterzeichner',
+	'titre_signatures_publie' => 'Bestätigte Unterzeichner',
+	'titre_suivi_petition' => 'Petitionen verfolgen',
+	'tout_voir' => 'Alle Unterzeichner anzeigen',
 
 	// U
-	'une_signature' => '1 signature', # NEW
+	'une_signature' => '1 Unterzeichner',
 
 	// V
-	'voir_signatures_objet' => 'Voir les signatures' # NEW
+	'voir_signatures_objet' => 'Unterzeichner Anzeigen '
 );
 
 ?>
