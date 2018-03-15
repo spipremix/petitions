931ca21662adbd67080abaf2c4b9c792745c36a3
gpl <gpl@long-player.net>
2018-03-15 04:42:06 +0000

    [Salvatore] [source:_core_/plugins/petitions/lang/ petitions] Export depuis http://trad.spip.net de la langue eo

--- lang/petitions_eo.php
+++ lang/petitions_eo.php
@@ -9,18 +9,27 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'alerte_confirmer_suppression_signatures' => 'Vi estas forigonta ĉiujn subskribojn de tiu petskribo. Ĉu vi volas vere plui ?',
+	'aucune_signature' => 'Neniu subskribo',
+
 	// B
+	'bouton_checkbox_envoi_message' => 'ebleco sendi mesaĝon',
+	'bouton_checkbox_indiquer_site' => 'indiki nepre retejon',
+	'bouton_checkbox_signature_unique_email' => 'nur unu subskribo por unu retpoŝtadreso',
+	'bouton_checkbox_signature_unique_site' => 'nur unu subskribo por retejo',
 	'bouton_radio_activer_petition' => 'Aktivigi la petskribon',
 	'bouton_radio_pas_petition' => 'Neniu petskribo',
 	'bouton_radio_petition_activee' => 'Aktivigita petskribo',
 	'bouton_radio_supprimer_petition' => 'Forviŝi la petskribon',
 
 	// F
-	'form_pet_adresse_site' => 'Retadreso de via retejo', # MODIF
-	'form_pet_aucune_signature' => 'Neniu subskribo kongruas kun tiu kodo...', # MODIF
+	'filtre' => 'Filtrilo :',
+	'form_pet_adresse_site' => 'Retadreso de via retejo',
+	'form_pet_aucune_signature' => 'Neniu subskribo kongruas kun tiu kodo...',
 	'form_pet_confirmation' => 'Bonvolu konfirmi vian subskribon :',
 	'form_pet_deja_signe' => 'Vi jam subskribis tiun tekston.',
-	'form_pet_envoi_mail_confirmation' => 'Konfirma retmesaĝo estis ĵus sendita al @email@. Vi devas aliri la retadreson menciitan en tiu mesaĝo por validigi vian subskribon.', # MODIF
+	'form_pet_envoi_mail_confirmation' => 'Konfirma retmesaĝo estis ĵus sendita al @email@. Vi devas aliri la retadreson menciitan en tiu mesaĝo por validigi vian subskribon.',
 	'form_pet_mail_confirmation' => 'Saluton,
 
 Vi petis subskribi la petskribon :
@@ -41,22 +50,68 @@ estos rifuzita) :
 
 Dankon pro via partopreno
 ',
-	'form_pet_nom_site2' => 'Nomo de via retejo', # MODIF
+	'form_pet_message_commentaire' => 'Mesaĝo, komento ?',
+	'form_pet_nom_site2' => 'Nomo de via retejo',
 	'form_pet_probleme_liens' => 'Bonvolu retrakti vian mesaĝon forigante hiperteskajn ligilojn.',
 	'form_pet_probleme_technique' => 'Teknika problemo, la subskribeblo estas portempe ne ebla.',
 	'form_pet_signature_validee' => 'Via subskribo estas validigita. Dankon !',
 	'form_pet_site_deja_enregistre' => 'Tiu retejo estas jam registrita',
 	'form_pet_url_invalide' => 'La retadreso, kiun vi indikis, ne validas.',
 	'form_pet_votre_email' => 'Via retpoŝtadreso',
-	'form_pet_votre_nom' => 'Via nomo aŭ salutnomo', # MODIF
+	'form_pet_votre_nom' => 'Via nomo',
 	'form_pet_votre_site' => 'Se vi havas retejon, vi povas ĝin indiki ĉi-sube',
 
 	// I
+	'icone_relancer_signataire' => 'Re-peti la subskribinton',
+	'icone_relancer_signataires' => 'Re-peti la subskribintojn',
+	'icone_suivi_petitions' => 'Sekvi / mastrumi la petskribojn',
+	'icone_supprimer_signature' => 'Forigi tiun subskribon',
+	'icone_supprimer_signatures' => 'Forigi la subskribojn',
+	'icone_valider_signature' => 'Validigi tiun subskribon',
+	'icone_valider_signatures' => 'Validigi la subskribojn',
 	'info_adresse_email' => 'RETPOŜTADRESO :',
+	'info_fonctionnement_petition' => 'Kiel funkcias la petskribo :',
 	'info_signature_supprimee' => 'Subskribo forviŝita',
-	'info_signature_supprimee_erreur' => 'Eraro : tiu ĉi forviŝcela kodo kongruas kun neniu subskribo', # MODIF
+	'info_signature_supprimee_erreur' => 'Eraro : tiu ĉi forviŝcela kodo kongruas kun neniu subskribo',
 	'info_site_web' => 'RETEJO :',
+	'info_texte_message' => 'Via mesaĝoteksto :',
 
 	// L
-	'lien_reponse_article' => 'Respondo al la artikolo'
+	'lien_reponse_article' => 'Respondo al la artikolo',
+
+	// N
+	'nombre_signatures' => '@nb@ subskriboj',
+
+	// S
+	'sans_nom' => 'Anonima',
+	'signatures_article' => 'Subskriboj de la artikolo :',
+	'signatures_aucune' => 'Neniu',
+	'signatures_meme_auteur' => 'La subskriboj de la sama aŭtoro',
+	'signatures_meme_site' => 'La subskriboj kun la sama retejo',
+	'signatures_poubelle' => 'Forigitaj',
+	'signatures_prop' => 'Atendante',
+	'signatures_publie' => 'Konfirmitaj',
+	'signatures_recherche_label' => 'Serĉi subskribojn :',
+	'signatures_selection' => 'Elekti la subskribojn :',
+	'signatures_toutes' => 'Ĉiuj',
+	'statut_poubelle' => 'Forigita',
+	'statut_prop' => 'Atendante validigon',
+	'statut_publie' => 'Konfirmita',
+
+	// T
+	'texte_descriptif_petition' => 'Priskribo de la petskribo',
+	'titre_page_controle_petition' => 'Spurado de la petskriboj',
+	'titre_petition' => 'Petskribo',
+	'titre_petitions' => 'Petskriboj',
+	'titre_selection_action' => 'Elekto',
+	'titre_signatures_attente' => 'Subskriboj, pri kiuj oni atendas validigon',
+	'titre_signatures_publie' => 'Konfirmitaj subskriboj',
+	'titre_suivi_petition' => 'Spurado de la petskriboj',
+	'tout_voir' => 'Vidi ĉiujn subskribojn',
+
+	// U
+	'une_signature' => '1 subskribo',
+
+	// V
+	'voir_signatures_objet' => 'Vidi la subskribojn'
 );
