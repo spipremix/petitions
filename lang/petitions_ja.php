7570bd954293c7dba4a2b68f0b8f8ef42e0d4ca1
origenius <origenius@mvb.biglobe.ne.jp>
2018-09-28 03:40:08 +0000

    [Salvatore] [source:_core_/plugins/petitions/lang/ petitions] Export depuis http://trad.spip.net de la langue ja

--- lang/petitions_ja.php
+++ lang/petitions_ja.php
@@ -9,47 +9,106 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
+	// A
+	'alerte_confirmer_suppression_signatures' => 'この請願の署名をすべて削除することになります。 続行してもよろしいですか？',
+	'aucune_signature' => '署名なし',
+
 	// B
-	'bouton_radio_activer_petition' => '署名機能を使う',
-	'bouton_radio_pas_petition' => '署名機能なし',
-	'bouton_radio_petition_activee' => '署名機能を使用中',
-	'bouton_radio_supprimer_petition' => '署名機能を無効にする',
+	'bouton_checkbox_envoi_message' => 'メッセージを送信する可能性',
+	'bouton_checkbox_indiquer_site' => 'サイトの名前を入力する必要があります。',
+	'bouton_checkbox_signature_unique_email' => 'Ｅメールごとに1つの署名のみ',
+	'bouton_checkbox_signature_unique_site' => 'ウェブサイトごとに1つの署名のみ',
+	'bouton_radio_activer_petition' => '請願を有効にする',
+	'bouton_radio_pas_petition' => '請願無し',
+	'bouton_radio_petition_activee' => '請願が有効になりました。',
+	'bouton_radio_supprimer_petition' => '請願を削除する',
 
 	// F
-	'form_pet_adresse_site' => 'あなたのサイトのURL', # MODIF
-	'form_pet_aucune_signature' => '署名がこのコードと一致しません...', # MODIF
-	'form_pet_confirmation' => 'あなたの署名を確認してください:',
-	'form_pet_deja_signe' => 'あなたは既にこの文章に署名しました。',
-	'form_pet_envoi_mail_confirmation' => '確認用メールが今、あなたへ送られました。記述されたメールアドレスが、あなたのものだと証明するため、メールに記載してあるURLを開いてください。', # MODIF
-	'form_pet_mail_confirmation' => 'ご機嫌いかがですか？
+	'filtre' => 'フィルター：',
+	'form_pet_adresse_site' => 'あなたのサイトのURL',
+	'form_pet_aucune_signature' => '署名がこのコードと一致しません...',
+	'form_pet_confirmation' => '署名を確認してください：',
+	'form_pet_deja_signe' => '既にこの文章に署名しました。',
+	'form_pet_envoi_mail_confirmation' => '確認メールが@email@に送信されました。このＥメールに記載されているURLにアクセ
+スして署名を確認する必要があります。',
+	'form_pet_mail_confirmation' => 'こんにちは、
 
-あなたが署名を提出しました:
-@titre@.
+あなたは、請願@titre@に署名を追加するように願いました。
 
-次の情報が提出されました:
-    名前: @nom_email@
-    サイト: @nom_site@ - @url_site@
+次の情報を提供しました。
+ お名前：@nom_email@
+ サイト：@nom_site@  -  @url_site@
+@message@
 
 重要...
-署名を有効にするために、今すぐ下のホームページを開いてください
- (さもなければ、メールアドレスを証明できないため、あなたの署名は拒絶されるでしょう):
-    @url@
+署名を検証するには、以下のURLに接続するだけです。
+（そうしてくださらないと、ご要求は拒否されます。）
 
-参加してくれてありがとうございました。
-', # MODIF
-	'form_pet_nom_site2' => 'あなたのウェブサイトの名前', # MODIF
-	'form_pet_probleme_technique' => '技術的問題、署名はほんの短い間、中断されました。',
-	'form_pet_signature_validee' => 'あなたの署名は有効です。Thank you!',
+@url@
+
+ご協力、ありがとうございました。！',
+	'form_pet_message_commentaire' => 'よろしければ、メッセージをお寄せください。',
+	'form_pet_nom_site2' => 'あなたのウェブサイトの名前',
+	'form_pet_probleme_liens' => 'ハイパーリンクを削除してメッセージを修正してください。',
+	'form_pet_probleme_technique' => '技術的な問題：署名は一時的に中断されます。',
+	'form_pet_signature_validee' => 'あなたの署名は有効になりました。ありがとうございました。',
 	'form_pet_site_deja_enregistre' => 'このサイトは既に登録されています。',
 	'form_pet_url_invalide' => 'あなたが入力したURLは、有効ではありません。',
-	'form_pet_votre_email' => 'あなたのe-mailアドレス',
-	'form_pet_votre_nom' => 'あなたの名前か別名', # MODIF
-	'form_pet_votre_site' => 'もしあなたがウェブサイトを持っているなら、下に入力することが出来ます。',
+	'form_pet_votre_email' => 'あなたのＥメールアドレス',
+	'form_pet_votre_nom' => 'お名前',
+	'form_pet_votre_site' => 'ウェブサイトを持っているならば、下に入力することが出来ます。',
 
 	// I
+	'icone_relancer_signataire' => '署名者を再度連絡する',
+	'icone_relancer_signataires' => '署名者を再度連絡する',
+	'icone_suivi_petitions' => '請願を管理する',
+	'icone_supprimer_signature' => '署名を削除する',
+	'icone_supprimer_signatures' => '署名の削除する',
+	'icone_valider_signature' => '署名を確認する',
+	'icone_valider_signatures' => '署名を確認する',
 	'info_adresse_email' => 'メールアドレス：',
+	'info_fonctionnement_petition' => '請願の仕組み:',
+	'info_signature_supprimee' => '削除された署名',
+	'info_signature_supprimee_erreur' => 'エラー：この削除コードは署名と一致しません。',
 	'info_site_web' => 'ウェブサイト：',
+	'info_texte_message' => 'メッセージのテキスト',
 
 	// L
-	'lien_reponse_article' => '記事へ返信'
+	'lien_reponse_article' => '記事への返信',
+
+	// N
+	'nombre_signatures' => '署名@nb@件',
+
+	// S
+	'sans_nom' => '匿名',
+	'signatures_article' => '記事の署名：',
+	'signatures_aucune' => '無し',
+	'signatures_meme_auteur' => '同じ作者の署名',
+	'signatures_meme_site' => '同じサイトの署名',
+	'signatures_poubelle' => '削除',
+	'signatures_prop' => '処理中',
+	'signatures_publie' => '確認',
+	'signatures_recherche_label' => '署名の検索：',
+	'signatures_selection' => '署名の選択：',
+	'signatures_toutes' => '全',
+	'statut_poubelle' => '削除',
+	'statut_prop' => '確認中',
+	'statut_publie' => '確認',
+
+	// T
+	'texte_descriptif_petition' => '請願の説明',
+	'titre_page_controle_petition' => '請願の管理',
+	'titre_petition' => '請願',
+	'titre_petitions' => '請願',
+	'titre_selection_action' => '選択',
+	'titre_signatures_attente' => '署名確認中',
+	'titre_signatures_publie' => '確認された署名',
+	'titre_suivi_petition' => '請願の管理',
+	'tout_voir' => 'すべての署名を見る',
+
+	// U
+	'une_signature' => '署名一件',
+
+	// V
+	'voir_signatures_objet' => '署名を見る'
 );
